1436d831b72d8671af1ecea263de3455b12327f1
VitorCarvalho1 <vitorhugo.carvalho2@yahoo.com.br>
2022-08-06 02:44:53 -0300

    add array lendas

---
+++ scripts.js
@@ -0,0 +1,82 @@
+
+
+//const nome = lendas[0].nome
+//const name = document.getElementById('name').innerText = nome
+
+const lendas = [
+    {nome: "Renee Blasey", idade: 32, planeta: "Typhon", HabilidadeTatica: "Adentrando o Vazio",
+    HabilidadePassiva: "Vozes do Vazio", HabilidadeSuprema: "Fenda Dimensional"},
+
+    {nome: "Dra. Ashleigh Reid", idade: 121, planeta: "Desconhecido", HabilidadeTatica: "Armadilha Voltaica",
+    HabilidadePassiva: "Marca da Morte", HabilidadeSuprema: "Invasão Dimensional"},
+
+    {nome: "Anita Williams", idade:	38, planeta: "Gridiron", HabilidadeTatica: "Lançador de Fumaça",
+    HabilidadePassiva: "Asas nos pés", HabilidadeSuprema: "Estrondo do trovão"},
+
+    {nome: "Desconhecido", idade: "Desconhecido", planeta: "Talos", HabilidadeTatica: "Olho do Pai de Todos",
+    HabilidadePassiva: "Rastreador", HabilidadeSuprema: "Fera da Caça"},
+
+    {nome: "Alexander Nox", idade: 48, planeta: "Gaea", HabilidadeTatica: "Armadilha de Gás Nox",
+    HabilidadePassiva: "Visão Nox", HabilidadeSuprema: "Granada de Gás Nox"},
+
+    {nome: "Tae Joon Park", idade: 31, planeta: "Gaea", HabilidadeTatica: "Drone de Vigilância",
+    HabilidadePassiva: "Neurolink", HabilidadeSuprema: "PEM do Drone"},
+
+    {nome: "Walter Fitzroy", idade: 54, planeta: "Salvo", HabilidadeTatica: "Bomba de Fragmentação",
+    HabilidadePassiva: "Granadeiro", HabilidadeSuprema: "Megabomba"},
+
+    {nome: "Makoa Gibraltar", idade: 30, planeta: "Solace", HabilidadeTatica: "Domo de Proteção",
+    HabilidadePassiva: "Escudo de Arma", HabilidadeSuprema: "Bombardeio Defensivo"},
+
+    {nome: "Dra. Mary Somers", idade: "37 (com margem de erro de 88 anos...)", planeta: "Psamathe", HabilidadeTatica: "Elevador Gravitacional",
+    HabilidadePassiva: "Andar Espacial", HabilidadeSuprema: "Buraco Negro"},
+
+    {nome: "Ajay Che", idade: 24, planeta: "Psamathe", HabilidadeTatica: "Drone Dr. Cura",
+    HabilidadePassiva: "Médica de Combate", HabilidadeSuprema: "Cápsula de Suprimentos"},
+
+    {nome: "Loba Andrade", idade: 34, planeta: "Nenhum", HabilidadeTatica: "Melhor Amigo da Ladra",
+    HabilidadePassiva: "De Olho na Qualidade", HabilidadeSuprema: "Butique do Mercado Clandestino"},
+
+    {nome: "Margaret Kōhere", idade: 55, planeta: "Salvo", HabilidadeTatica: "Furadeira de Motim",
+    HabilidadePassiva: "Cólera Déspota", HabilidadeSuprema: "Bola de Demolição"},
+
+    {nome: "Elliott Witt", idade: 30, planeta: "Solace", HabilidadeTatica: "Astúcia",
+    HabilidadePassiva: "Agora Você Me Vê...", HabilidadeSuprema: "Animação da Festa"},
+
+    {nome: "Jackson Williams", idade: 40, planeta: "Gridiron", HabilidadeTatica: "Escudo Móvel",
+    HabilidadePassiva: "Resgate de Feridos", HabilidadeSuprema: "Muralha do Castelo"},
+
+    {nome: "Octavio Silva", idade: 24, planeta: "Psamathe", HabilidadeTatica: "Estimulante",
+    HabilidadePassiva: "Reparo Rápido", HabilidadeSuprema: "Suporte de Salto"},
+
+    {nome: "MRVN", idade: "Ele quer muito saber também.", planeta: "Ele está tentando descobrir!", HabilidadeTatica: "Gancho",
+    HabilidadePassiva: "Fontes Internas", HabilidadeSuprema: "Arma de Tirolesa"},
+
+    {nome: "Ramya Parekh", idade: 21, planeta: "Gaea", HabilidadeTatica: "Cobertura Amplificada",
+    HabilidadePassiva: "Carregador Modificado", HabilidadeSuprema: "Sheila - Minigun Estacionária"},
+
+    {nome: "Desconhecido", idade: "Desconhecido", planeta: "Solace", HabilidadeTatica: "	Silêncio",
+    HabilidadePassiva: "Stalker", HabilidadeSuprema: "Totem da Morte"},
+
+    {nome: "Obi Edolasim", idade: 26, planeta: "Boreas", HabilidadeTatica: "Foco de Atenção",
+    HabilidadePassiva: "Busca de Coração", HabilidadeSuprema: "Exposição"},
+
+    {nome: "Kairi Imahara", idade: 30, planeta: "Angelia", HabilidadeTatica: "Enxame de Mísseis",
+    HabilidadePassiva: "Jatos VTOL", HabilidadeSuprema: "Salto Ascendente"},
+
+    {nome: "Xiomara", idade: 18, planeta: "Págos", HabilidadeTatica: "Reposicionamento do Echo",
+    HabilidadePassiva: "Lente de Observação", HabilidadeSuprema: "Marca de Franco-atiradora"},
+
+    {nome: "Natalie Paquette", idade: 22, planeta: "Solace", HabilidadeTatica: "Segurança de Perímetro",
+    HabilidadePassiva: "Carregamento Relâmpago", HabilidadeSuprema: "Torre Interceptora"},
+]
+
+
+
+
+
+
+ 
+
+
+
