e66923fd9c597225f9f4ce84de9a8be668f4250a
VitorCarvalho1 <vitorhugo.carvalho2@yahoo.com.br>
2022-08-07 01:33:19 -0300

    add ScrollReveal

---
+++ ScrollReveal.js
@@ -0,0 +1,15 @@
+
+window.sr = ScrollReveal({ reset: true }); 
+
+
+sr.reveal('.divisao', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 2000
+});
+
+sr.reveal('li', {
+    rotate: { x: 0, y: 100, z: 0},
+    duration: 1000,
+})
+
+
